e09e403daef1c43927ff715f887d923be03898d4
Jireh Battung <jirehgbattung@gmail.com>
2022-08-19 17:59:32 +0800

    added mode testing and fixed index edit link

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
     validates :title, presence: true, 
-                    uniqueness: true
-    validates :details, presence: true
+                    uniqueness: true,
+                    length: {maximum: 20}
+    validates :details, presence: true,
+                    length: {minimum: 10, maximum: 100}
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
+
+  test "should be able to create a new category" do
+    category = Category.new
+    category.title = "Web Development"
+    category.details = "activities related to programming and web development"
+    assert category.save, "Created a new category"
+  end
   
   test "should not save without title" do
     category = Category.new
@@ -8,11 +15,11 @@ class CategoryTest < ActiveSupport::TestCase
     assert_not category.save, "Saved the category without a title"
   end
 
-  test "should be able to create a new category" do
+  test "should be able to reject a category title with more than 20 characters" do
     category = Category.new
-    category.title = "Web Development"
+    category.title = "Web Development Bootcamp Avion School"
     category.details = "activities related to programming and web development"
-    assert category.save, "Created a new category"
+    assert_not category.save, "Saved a title with more than 20 characters"
   end
 
   test "should not be able to save duplicate title names" do
@@ -31,7 +38,21 @@ class CategoryTest < ActiveSupport::TestCase
   test "should not be able to save without details" do
     category = Category.new
     category.title = "Web Development"
-    assert_not category.save, "Saved the category without a description"
+    assert_not category.save, "Saved the category without details"
+  end
+
+  test "should be able to reject details with less than 10 characters" do
+    category = Category.new
+    category.title = "Web Development"
+    category.details = "Random"
+    assert_not category.save, "Added details with less than 10 characters"
+  end
+
+  test "should be able to reject details with more than 100 characters" do
+    category = Category.new
+    category.title = "Web Development"
+    category.details = "Random is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    assert_not category.save, "Added details with more than 100 characters"
   end
 
 end
