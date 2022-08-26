c2ca561411194c718be23435e2b8585c007a43ca
Jireh Battung <jirehgbattung@gmail.com>
2022-08-26 15:43:46 +0800

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  # get 'home/index'
   devise_for :users
+  root 'home#index'
   get '/today' => 'tasks#today', as: 'today'
   resources :categories do
     resources :tasks
